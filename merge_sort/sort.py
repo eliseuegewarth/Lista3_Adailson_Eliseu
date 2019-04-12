a5a5a50dbbd20fbcfa1dbd2cb0cb9cd0af3d1f14
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-04-12 14:41:26 -0300

    Adicionando USAGE cli feedback
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- merge_sort/sort.py
+++ merge_sort/sort.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 import time
 import sys
 from gera_vector import gera_vector_rapido
@@ -56,5 +57,8 @@ if __name__ == '__main__':
             pass
         else:
             pass
+    elif(len(sys.argv) == 2):
+        if sys.argv[1] == "--help":
+            print("USAGE:\n\t{} <sort_strategy> <iterations_per_benchmark> <start_with_n_elements> <end_with_n_elements> <range_step> <new_benchmark>".format(sys.argv[0]))
     else:
         main()
