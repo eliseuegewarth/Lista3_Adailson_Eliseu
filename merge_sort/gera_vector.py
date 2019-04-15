bdf42a22b05540e1f661fefaf5d9dcb339cde29a
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-04-15 11:01:51 -0300

    Atualizando plot_benchmark de gera_vector
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- merge_sort/gera_vector.py
+++ merge_sort/gera_vector.py
@@ -33,7 +33,7 @@ def gera_vector_rapido(int_range, count):
 
     return vector
 
-def single_run_benchmark(numbers=10000000):
+def test_gera_vetor(numbers=10000000):
     import time
     duration = {}
     numbers = int(numbers)
@@ -45,7 +45,7 @@ def single_run_benchmark(numbers=10000000):
     end=time.time()
     if len(r) != numbers:
          raise Exception("vector size ({}) is lower than number of elements ({})\n".format(len(r), numbers))
-    duration['sync'] = (end - start)
+    duration['sync'] = int(1000*1000*(end - start))/1000
 
     r = []
     start = time.time()
@@ -53,7 +53,7 @@ def single_run_benchmark(numbers=10000000):
     end=time.time()
     if len(r) != numbers:
          raise Exception("vector size ({}) is lower than number of elements ({})\n".format(len(r), numbers))
-    duration['async'] = (end - start)
+    duration['async'] = int(1000*1000*(end - start))/1000
 
     return duration
 
@@ -61,14 +61,17 @@ def plot_run_benchmark():
     x = []
     y_async = []
     y_sync = []
-    for i in range(1000, 10000000, 1000000):
-        benchmark = single_run_benchmark(i)
+    start_range_size = 1000
+    end_range_size = 1000000
+    step_size = end_range_size//10
+    for i in range(start_range_size, end_range_size+1, step_size):
+        benchmark = test_gera_vetor(i)
         x.append(i)
         y_async.append(benchmark['async'])
         y_sync.append(benchmark['sync'])
 
     fig, ax = plt.subplots()
-    ax.plot(x, y_async, '*g', label='async')
+    ax.plot(x, y_async, '--g', label='async')
     ax.plot(x, y_sync, '-b', label='sync')
 
     plt.show()
