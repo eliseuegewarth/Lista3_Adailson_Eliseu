0e68ab4b154a1b9e0a3eb4d2c44c3da33a468421
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-04-16 17:34:58 -0300

    Otimizando plot dos datasets
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- merge_sort/plot_benchmark.py
+++ merge_sort/plot_benchmark.py
@@ -31,7 +31,6 @@ def plot_benchmark(dataframes, *args):
 
 if __name__ == '__main__':
     files = []
-    # import ipdb;ipdb.set_trace()
     if len(sys.argv) > 1:
         if sys.argv[1] == '__all__':
             mypath = "."
@@ -43,27 +42,33 @@ if __name__ == '__main__':
     else:
         files.append('benchmark_recursive_merge_sort.csv')
     file_dfs = []
+    legends_dfs = []
     df_medians = []
+    legends_dfs_medians = []
     for file in files:
         df_median = []
         df = get_dataset(file)
         df = sort_by_columns(df, ['dataset_size','time_in_ms'])
+
+        dataset_file_name = "_".join(file.split('.')[0].split('_')[1:7])
+        median_of = "{}_median".format(dataset_file_name)
+
         for i in range(df.dataset_size.unique().size):
-            median_of = "{}_median".format("_".join(file.split('_')[1:8]))
             df_median.append([median_of, df.dataset_size.unique()[i], np.median(df[df.dataset_size == df.dataset_size.unique()[i]].iloc[:,2].values), True])
+
         file_dfs.append(df)
+
         df_median = pd.DataFrame(df_median, columns=["strategy", "dataset_size", "time_in_ms", "is_random"])
         df_median = sort_by_columns(df_median, ['dataset_size','time_in_ms'])
         df_medians.append(df_median)
+
+        legends_dfs.append(dataset_file_name)
+        legends_dfs_medians.append(median_of)
+
     dfs = file_dfs
-    x_medians_meeting = 0
-    y_medians_meeting = 0
-    for i in range(len(df_medians[0])):
-        if df_medians[0].iloc[i][2] < df_medians[1].iloc[i][2]:
-            x_medians_meeting = df_medians[0].iloc[i][1]
-            y_medians_meeting = df_medians[0].iloc[i][2]
-            break
-    ax = plot_benchmark(dataframes=dfs+df_medians)
-    ax.plot(x_medians_meeting, y_medians_meeting, 'ko')
-    ax.legend([df.strategy.unique()[0] for df in dfs+df_medians]+ ['crosspoint'])
+
+    data_figure = plot_benchmark(dataframes=dfs)
+    data_figure.legend(legends_dfs)
+    data_medians_figure = plot_benchmark(dataframes=df_medians)
+    data_medians_figure.legend(legends_dfs_medians)
     plt.show()
